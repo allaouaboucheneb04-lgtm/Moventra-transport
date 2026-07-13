38474380cb280268e560076b42c13e6fca87f090
Allaoua Boucheneb <allaouaboucheneb04@gmail.com>
2026-07-13 01:18:16 -0400

    fire

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/fireba
 import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCz1BC9YSHQsBB1kBRWy8TdLqs2D7ytOiA",
-  authDomain: "dedie-menage.firebaseapp.com",
-  projectId: "dedie-menage",
-  storageBucket: "dedie-menage.firebasestorage.app",
-  messagingSenderId: "745599536988",
-  appId: "1:745599536988:web:c217af2d377cf70ffffb99"
-};
+  apiKey: "AIzaSyBeDOLjFyONjv06dUc4b_R0lQ4AlSBPU2U",
+  authDomain: "moventra-fe20f.firebaseapp.com",
+  projectId: "moventra-fe20f",
+  storageBucket: "moventra-fe20f.firebasestorage.app",
+  messagingSenderId: "634208041846",
+  appId: "1:634208041846:web:37bf64bed692efd5f31857",};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCz1BC9YSHQsBB1kBRWy8TdLqs2D7ytOiA",
-  authDomain: "dedie-menage.firebaseapp.com",
-  projectId: "dedie-menage",
-  storageBucket: "dedie-menage.firebasestorage.app",
-  messagingSenderId: "745599536988",
-  appId: "1:745599536988:web:c217af2d377cf70ffffb99"
+  apiKey: "AIzaSyBeDOLjFyONjv06dUc4b_R0lQ4AlSBPU2U",
+  authDomain: "moventra-fe20f.firebaseapp.com",
+  projectId: "moventra-fe20f",
+  storageBucket: "moventra-fe20f.firebasestorage.app",
+  messagingSenderId: "634208041846",
+  appId: "1:634208041846:web:37bf64bed692efd5f31857",
+  measurementId: "G-N0LFJ6JQXB"
 };
 
 export const ROLES_COLLECTION = "users";
@@ -15,4 +16,4 @@ export const TASKS_COLLECTION = "taches";
 // IMPORTANT: Remplace par ta clé VAPID Firebase Cloud Messaging.
 // Firebase Console > Project settings > Cloud Messaging > Web Push certificates > Generate key pair
 
-export const VAPID_KEY = "BP11TmYg0QdKbduM_KDvbLoYhjAHIND9MtRkc7A30_tAgOWys7jyopBYN5lYMYlx96v5X4fgjJcbO3Y_6t3ezjs";
+export const VAPID_KEY = ""; // À générer dans Firebase Moventra si FCM est utilisé
